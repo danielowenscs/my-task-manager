5ff04fbeea6a3f04f7dc9b8dceb3175964b647d4
Daniel Owens <danielowenscs@gmail.com>
2025-07-28 19:26:11 -0400

    added hard coded list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,19 @@
 import TodoItem from './TodoItem'
 
-export default function TodoList({ todos = [], onToggle, onDelete }) {
+const defaultTodos = [
+  { id: 1, text: 'Learn React', completed: false },
+  { id: 2, text: 'Build a Todo app', completed: true },
+]
+
+function onToggle(id) {
+  console.log('Toggle todo with id:', id)
+}
+
+function onDelete(id) {
+  console.log('Delete todo with id:', id)
+}
+
+export default function TodoList({ todos = defaultTodos }) {
   return (
     <div className="border rounded-md mt-4">
       {todos.map(todo => (
