68461d02a9d4c17cd9b825d3ad147d5058d4b77f
Daniel Owens <danielowenscs@gmail.com>
2025-07-30 19:47:36 -0400

    added styles to nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,12 +16,13 @@ export default function Navbar() {
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <div className={styles.leftLinks}>
+        <div className={styles.left}>
           <Link to="/" className={styles.link}>Home</Link>
           <Link to="/about" className={styles.link}>About</Link>
-        </div>
-        <div className={styles.rightLinks}>
           {!user && <Link to="/login" className={styles.link}>Login</Link>}
+        </div>
+        <h1 className={styles.logo}>To Do Manager</h1>
+        <div className={styles.right}>
           {user && (
             <button onClick={handleLogout} className={styles.button}>
               Logout
